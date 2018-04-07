c6d01c28e104014f95e5a93ef589672f86f4889d
ivanlmj <ivanlmj@gmail.com>
2018-04-07 17:31:35 -0500

    adding base module

---
+++ tulian.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Extracts data from tuples and lists. """
+
+__author__ = "@ivanleoncz"
+
+
+class Tulian:
+
+    def __init__(self,lst):
+        self.lst = lst
+    
+
+    def numbers(self):
+        """ Maps all numbers, using list comprehensions with filter. 
+        
+        Returns a list. """
+        return [n for n in self.lst if type(n) == int]
+
+
+    def strings(self):
+        """ Maps all strings, using list comprehensions with filter. 
+        
+        Returns a list. """
+        return [s for s in self.lst if type(s) == str]
+
+
+    def dicts(self):
+        """ Maps all dicts, using list comprehensions with filter. 
+        
+        Returns a list. """
+        return [d for d in self.lst if type(d) == dict]
+
+
+    def tuples(self):
+        """ Maps all tuples, using list comprehensions with filter. 
+        
+        Returns a list. """
+        return [t for t in self.lst if type(t) == tuple]
