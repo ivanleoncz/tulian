cc19b212c9114984c1e603ee0578be11f772fc65
ivanlmj <ivanlmj@gmail.com>
2018-04-22 18:34:24 -0500

    decalring encoding utf-8 + adding module docstring

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Tulian
+    ~~~~~~
+
+    Insights from Tuples and Lists, using Comprehensions.
+
+    Tulian provides a summary (JSON) of data types present on such source.
+    It also provides the extraction/presentation of a specific data type.
+
+    For more information: https://github.com/ivanlmj/tulian
+"""
 
 __author__ = "@ivanleoncz"                                                      
 __version__ = "1.0"
