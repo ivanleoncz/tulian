da9ef9f706a1b86728b621244c637483904c0872
ivanlmj <ivanlmj@gmail.com>
2018-04-22 19:06:50 -0500

    adjusting docstring

--- __init__.py
+++ __init__.py
@@ -5,7 +5,8 @@
 
     Insights from Tuples and Lists, using Comprehensions.
 
-    Tulian provides a summary (JSON) of data types present on such source.
+    Tulian provides a summary (JSON) of data types contained in a Tuple 
+    or in a List.
     It also provides the extraction/presentation of a specific data type.
 
     For more information: https://github.com/ivanlmj/tulian
